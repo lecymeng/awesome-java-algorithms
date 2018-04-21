d6447d98924482bf4f2b4b2fb893b6b38e981895
lecymeng <1570682285@qq.com>
2018-04-21 12:47:18 +0800

    format

--- src/singleton/Singleton.java
+++ src/singleton/Singleton.java
@@ -14,12 +14,14 @@ public class Singleton {
      * 1.0
      * 单例模式,懒汉式,线程安全
      */
-    public static class Singleton1{
+    public static class Singleton1 {
         private final static Singleton1 INSTANCE = new Singleton1();
-        private Singleton1(){
+
+        private Singleton1() {
 
         }
-        private static Singleton1 getInstance(){
+
+        private static Singleton1 getInstance() {
             return INSTANCE;
         }
     }
@@ -28,14 +30,15 @@ public class Singleton {
      * 2.0
      * 单例模式,饿汉式,线程不安全
      */
-    public static class Singleton2{
+    public static class Singleton2 {
         private static Singleton2 instance = null;
-        private Singleton2(){
+
+        private Singleton2() {
 
         }
 
         public static Singleton2 getInstance() {
-            if (instance == null){
+            if (instance == null) {
                 instance = new Singleton2();
             }
             return instance;
@@ -46,11 +49,14 @@ public class Singleton {
      * 3.0
      * 单例模式,饿汉式,线程不安全,多线程环境下效率不高-synchronized
      **/
-    public static class Singleton3{
+    public static class Singleton3 {
         private static Singleton3 instance = null;
-        private Singleton3(){}
-        public static synchronized Singleton3 getInstance(){
-            if (instance == null){
+
+        private Singleton3() {
+        }
+
+        public static synchronized Singleton3 getInstance() {
+            if (instance == null) {
                 instance = new Singleton3();
             }
             return instance;
@@ -60,18 +66,21 @@ public class Singleton {
     /**
      * 4.0
      * 单例模式,饿汉式,变种-线程不安全
-     *
+     * <p>
      * 静态代码块
      **/
-    public static class Singleton4{
+    public static class Singleton4 {
         private static Singleton4 instance = null;
+
         static {
             instance = new Singleton4();
         }
-        private Singleton4(){
+
+        private Singleton4() {
 
         }
-        public static Singleton4 getInstance(){
+
+        public static Singleton4 getInstance() {
             return instance;
         }
     }
@@ -79,17 +88,19 @@ public class Singleton {
     /**
      * 5.0
      * 单例模式,使用静态内部类,线程安全[推荐]
-     *
+     * <p>
      * 静态内部类（构造函数不需要外部参数时）
      **/
-    private static class Singleton5{
-        private final static class SingletonHolder{
+    private static class Singleton5 {
+        private final static class SingletonHolder {
             private static final Singleton5 INSTANCE = new Singleton5();
         }
-        private Singleton5(){
+
+        private Singleton5() {
 
         }
-        public static Singleton5 getInstance(){
+
+        public static Singleton5 getInstance() {
             return SingletonHolder.INSTANCE;
         }
     }
@@ -98,9 +109,10 @@ public class Singleton {
      * 6.0
      * 静态内部类,枚举方式,线程安全[推荐]
      **/
-    public enum Singleton6{
+    public enum Singleton6 {
         INSTANCE;
-        public void whateverMethod(){
+
+        public void whateverMethod() {
 
         }
     }
@@ -109,13 +121,16 @@ public class Singleton {
      * 7.0
      * 静态内部类,使用双重校验锁,线程安全[推荐]
      **/
-    public static class Singleton7{
+    public static class Singleton7 {
         private volatile static Singleton7 instance = null;
-        private Singleton7(){}
-        public static Singleton7 getInstance(){
+
+        private Singleton7() {
+        }
+
+        public static Singleton7 getInstance() {
             if (instance == null) {
-                synchronized (Singleton7.class){
-                    if (instance == null){
+                synchronized (Singleton7.class) {
+                    if (instance == null) {
                         instance = new Singleton7();
                     }
                 }
