624b98bea0544d3af78e39b4b18cec4574b78fe2
lecymeng <1570682285@qq.com>
2018-04-22 12:16:36 +0800

    链表中倒数第k个结点

---
+++ src/SwordOffer/代码的鲁棒性/链表中倒数第k个结点/ListNode.java
@@ -0,0 +1,15 @@
+package SwordOffer.代码的鲁棒性.链表中倒数第k个结点;
+
+/**
+ * Created by Weicools on 2018/4/22.
+ * <p>
+ * desc:
+ */
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
