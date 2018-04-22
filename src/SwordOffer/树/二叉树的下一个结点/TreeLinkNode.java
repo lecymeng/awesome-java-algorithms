3f3985257f567b09d1f0bd900144c1ac9c3b9000
lecymeng <1570682285@qq.com>
2018-04-22 11:53:58 +0800

    二叉树

---
+++ src/SwordOffer/树/二叉树的下一个结点/TreeLinkNode.java
@@ -0,0 +1,17 @@
+package SwordOffer.树.二叉树的下一个结点;
+
+/**
+ * Created by Weicools on 2018/4/22.
+ * <p>
+ * desc:
+ */
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
---
+++ src/SwordOffer/树/重建二叉树/TreeNode.java
@@ -0,0 +1,16 @@
+package SwordOffer.树.重建二叉树;
+
+/**
+ * Created by Weicools on 2018/4/22.
+ * <p>
+ * desc:
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
