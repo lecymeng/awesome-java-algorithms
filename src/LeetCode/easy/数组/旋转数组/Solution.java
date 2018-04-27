5e0bef1921492db30bd4c891cf02543315702cdd
lecymeng <1570682285@qq.com>
2018-04-27 15:27:24 +0800

    旋转数组

---
+++ src/LeetCode/easy/数组/旋转数组/Solution.java
@@ -0,0 +1,34 @@
+package LeetCode.easy.数组.旋转数组;
+
+import java.util.Arrays;
+
+/**
+ * Created by Weicools on 2018/4/26.
+ * <p>
+ * desc:
+ */
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        solution.rotate(nums, 3);
+        System.out.println(Arrays.toString(nums));
+    }
+}
