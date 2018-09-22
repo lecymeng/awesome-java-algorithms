448eaef0a258626cb1ac5406dafaa711e5c10f0d
lecymeng <1570682285@qq.com>
2018-09-22 19:35:47 +0800

    merge array，palindrome2

--- src/LeetCode/easy/字符串/回文字符串2/Solution.java
+++ src/LeetCode/easy/字符串/回文字符串2/Solution.java
@@ -2,8 +2,13 @@ package LeetCode.easy.字符串.回文字符串2;
 
 /**
  * @author Weicools Create on 2018.09.19
- * <p>
- * desc: 可以删除一个字符，判断是否能构成回文字符串。
+ *
+ * 给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
+ * 示例 1:
+ * 输入: "aba" 输出: True 示例 2:
+ * 输入: "abca" 输出: True 解释: 你可以删除c字符。
+ *
+ * desc: https://leetcode-cn.com/problems/valid-palindrome-ii/description/
  */
 class Solution {
   public boolean validPalindrome (String s) {
