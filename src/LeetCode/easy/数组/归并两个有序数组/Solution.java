7834ea5a444d87dd235d10c5846ba9e9a3412e2a
lecymeng <1570682285@qq.com>
2018-10-01 13:37:11 +0800

    合并有序数组

--- src/LeetCode/easy/数组/归并两个有序数组/Solution.java
+++ src/LeetCode/easy/数组/归并两个有序数组/Solution.java
@@ -1,5 +1,7 @@
 package LeetCode.easy.数组.归并两个有序数组;
 
+import java.util.Arrays;
+
 /**
  * @author Weicools Create on 2018.09.21
  *
@@ -16,13 +18,27 @@ package LeetCode.easy.数组.归并两个有序数组;
  * desc: https://leetcode.com/problems/merge-sorted-array/description/
  */
 class Solution {
-  public void merge(int[] nums1, int m, int[] nums2, int n) {
-    int index1 = m-1,index2=n-1;
-    int indexMerge = m+n-1;
-    while (index1>=0||index2>=0){
-      if (index1<0){
-        nums1[indexMerge--]=nums2[index2--];
+  public void merge (int[] nums1, int m, int[] nums2, int n) {
+    int index1 = m - 1, index2 = n - 1;
+    int indexMerge = m + n - 1;
+    while (index1 >= 0 || index2 >= 0) {
+      if (index1 < 0) {
+        nums1[indexMerge--] = nums2[index2--];
+      } else if (index2 < 0) {
+        nums1[indexMerge--] = nums1[index1--];
+      } else if (nums1[index1] > nums2[index2]) {
+        nums1[indexMerge--] = nums1[index1--];
+      } else {
+        nums1[indexMerge--] = nums2[index2--];
       }
     }
+  }
+
+  public static void main (String[] args) {
+    Solution s = new Solution();
+    int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+    int[] nums2 = { 2, 5, 6 };
+    s.merge(nums1, 3, nums2, 3);
+    System.out.println(Arrays.toString(nums1));
   }
 }
