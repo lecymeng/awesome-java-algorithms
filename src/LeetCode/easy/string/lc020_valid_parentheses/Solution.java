df6f030854291a14ac447268a702fd52a214b23d
lecymeng <1570682285@qq.com>
2018-11-10 23:08:33 +0800

    add doc, add stack impl

--- src/LeetCode/easy/string/lc020_valid_parentheses/Solution.java
+++ src/LeetCode/easy/string/lc020_valid_parentheses/Solution.java
@@ -1,5 +1,7 @@
 package LeetCode.easy.string.lc020_valid_parentheses;
 
+import java.util.Stack;
+
 /**
  * @author Weicools Create on 2018.11.10
  *
@@ -29,6 +31,13 @@ package LeetCode.easy.string.lc020_valid_parentheses;
  * tag: 栈、字符串
  */
 class Solution {
+  /**
+   * 可以用栈来解决这个问题，
+   * 当出现左括号的时候入栈，当遇到右括号时，判断栈顶的左括号是否何其匹配，
+   * 不匹配的话直接返回 false 即可，最终判断是否空栈即可，
+   * 这里我们可以用数组模拟栈的操作使其操作更快，有个细节注意下 top = 1;，
+   * 从而省去了之后判空的操作和 top - 1 导致数组越界的错误
+   */
   public boolean isValid (String s) {
     char[] stack = new char[s.length() + 1];
     int top = 1;
@@ -47,11 +56,42 @@ class Solution {
     return top == 1;
   }
 
+  @Deprecated
+  public boolean isValidV0 (String s) {
+    if (s.isEmpty()) {
+      return true;
+    } else if (s.length() % 2 != 0) {
+      return false;
+    }
+
+    Stack<Character> stack = new Stack<>();
+    for (char c : s.toCharArray()) {
+      if (c == '(' || c == '[' || c == '{') {
+        stack.push(c);
+      } else if (c == ')' && !stack.isEmpty() && stack.pop() != '(') {
+        return false;
+      } else if (c == ']' && !stack.isEmpty() && stack.pop() != '[') {
+        return false;
+      } else if (c == '}' && !stack.isEmpty() && stack.pop() != '{') {
+        return false;
+      }
+    }
+
+    return stack.isEmpty();
+  }
+
   public static void main (String[] args) {
     Solution s = new Solution();
     System.out.println(s.isValid("{()}"));
     System.out.println(s.isValid("()[]{}"));
     System.out.println(s.isValid("()[{]}"));
     System.out.println(s.isValid("(({({"));
+
+    System.out.println();
+
+    System.out.println(s.isValidV0("{()}"));
+    System.out.println(s.isValidV0("()[]{}"));
+    System.out.println(s.isValidV0("()[{]}"));
+    System.out.println(s.isValidV0("(({({"));
   }
 }
