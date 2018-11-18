770fdd756a0a6e313a06d75814d34f77b3ed9a39
lecymeng <1570682285@qq.com>
2018-11-18 09:23:58 +0800

    lc019_remove_nth_node_from_end

--- src/LeetCode/easy/linked/lc019_remove_nth_node_from_end/Solution.java
+++ src/LeetCode/easy/linked/lc019_remove_nth_node_from_end/Solution.java
@@ -23,8 +23,39 @@ import LeetCode.easy.linked.ListNode;
  * tag: linked、双指针
  */
 class Solution {
-  public ListNode removeNthFromEnd(ListNode head, int n) {
-    return null;
+  public ListNode removeNthFromEnd (ListNode head, int n) {
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    ListNode first = head;
+    ListNode second = head;
+    for (int i = 0; i < n; i++) {
+      first = first.next;
+    }
+    while (first != null) {
+      first = first.next;
+      second = second.next;
+    }
+    second.next = second.next.next;
+    return head;
+  }
+
+  public ListNode removeNthFromEnd1(ListNode head, int n) {
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    int length  = 0;
+    ListNode first = head;
+    while (first != null) {
+      length++;
+      first = first.next;
+    }
+    length -= n;
+    first = dummy;
+    while (length > 0) {
+      length--;
+      first = first.next;
+    }
+    first.next = first.next.next;
+    return dummy.next;
   }
 
   public static void main (String[] args) {
